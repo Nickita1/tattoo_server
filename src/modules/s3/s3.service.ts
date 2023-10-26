bb4aecc0f8f67b3f0e95658d7a671b34ed767c56
Beast <your_email@example.com>
2023-10-26 23:32:52 +0300

    fixed s3 3

--- src/modules/s3/s3.service.ts
+++ src/modules/s3/s3.service.ts
@@ -18,7 +18,8 @@ export class S3Service {
       secretAccessKey: this.configService.get('AWS_USER_SECRET_ACCESS_KEY_ID'),
       region: this.configService.get('AWS_REGION'),
       params: this.configService.get('S3_BUCKET_NAME'),
-      useDualstackEndpoint: true
+      s3ForcePathStyle: true,
+      endpoint: 'http://localhost:9090'
     });
 
     const { filename, mimetype } = file;
