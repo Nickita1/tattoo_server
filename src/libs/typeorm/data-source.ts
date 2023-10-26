5434c6667017252c7f9b2f93b3deea0a2fac0f38
Beast <your_email@example.com>
2023-10-26 22:38:02 +0300

    fixed start 2

--- src/libs/typeorm/data-source.ts
+++ src/libs/typeorm/data-source.ts
@@ -2,28 +2,28 @@ import { DataSource } from 'typeorm';
 import { entities } from './entities';
 import { migrations } from './migrations';
 
-const dataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
-  entities,
-  migrations,
-  synchronize: true,
-});
-
 // const dataSource = new DataSource({
 //   type: 'postgres',
-//   host: 'dpg-cktauuo168ec73ca1gt0-a',
+//   host: 'localhost',
 //   port: 5432,
-//   username: 'postgres1',
-//   password: 'LJM0NT0qZa1ojK5mLXmCI1y7Aud6u2J1',
-//   database: 'tattoo_0pro',
+//   username: 'postgres',
+//   password: 'postgres',
+//   database: 'postgres',
 //   entities,
 //   migrations,
 //   synchronize: true,
 // });
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'dpg-cktauuo168ec73ca1gt0-a',
+  port: 5432,
+  username: 'postgres1',
+  password: 'LJM0NT0qZa1ojK5mLXmCI1y7Aud6u2J1',
+  database: 'tattoo_0pro',
+  entities,
+  migrations,
+  synchronize: true,
+});
 
 export default dataSource;
